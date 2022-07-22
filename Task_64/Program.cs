41906d94b53eedb11bb3af30b0bddb95b7d101c1
Maksim <maksim6erbinin@mail.ru>
2022-07-22 12:44:19 +0300

    Added console.write to task 64

--- Task_64/Program.cs
+++ Task_64/Program.cs
@@ -12,4 +12,5 @@ int numberM = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите натуральное число N: ");
 int numberN = Convert.ToInt32(Console.ReadLine());
 
+Console.Write("Натуральные числа от {0} до {1}: ", numberM, numberN);
 InterNumbers(numberM, numberN);
